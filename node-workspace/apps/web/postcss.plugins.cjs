f4cff471efb908bfabb7411d6f094e2d1e0fe344
Tanjim Hossain <tangimhossain1@gmail.com>
2022-09-02 17:58:40 +0600

    cloudflare pages

--- node-workspace/apps/web/postcss.plugins.cjs
+++ node-workspace/apps/web/postcss.plugins.cjs
@@ -1,16 +1,5 @@
 module.exports = () => {
     return {
-        postcssPlugin: 'postcss-will-change',
-        Declaration: {
-            'will-change': decl => {
-                let already = decl.parent.some(i => {
-                    return i.type === 'decl' && i.prop === 'backface-visibility'
-                })
-                if (!already) {
-                    decl.cloneBefore({ prop: 'backface-visibility', value: 'hidden' })
-                }
-            }
-        },
         postcssPlugin: 'postcss-fontvar-fallback',
         Declaration: {
             'font-weight': decl => {
