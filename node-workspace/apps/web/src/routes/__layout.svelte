7af2c7ce0456cb9053e24492695b54a77ac6c2b0
Tanjim Hossain <tangimhossain1@gmail.com>
2022-09-03 19:46:52 +0600

    overlay layer scroll fixed

--- node-workspace/apps/web/src/routes/__layout.svelte
+++ node-workspace/apps/web/src/routes/__layout.svelte
@@ -14,88 +14,105 @@
 	const pinned_nav_items = [{ title: 'Join In !! ヽ(⌣◡⌣”)ﾉ', link: './' }];
 	const history_nav_items = [{ title: '~', link: './' }];
 	const recommended_nav_items = [
-		{ title: 'Drop Me Anything!', subtitle: 'A Message? Link? Existential dread?', link: './' },
+		{
+			title: 'Drop Me Anything!',
+			subtitle: 'Anonymously.. ( ͡° ͜ʖ ͡°)',
+			link: './'
+		},
 		{ title: 'Hire Me!', subtitle: 'As Dev? Designer? Assassin?', link: './' },
-		{ title: 'Buy Me A Coffee UwU', subtitle: 'For all the boolsh*t i do', link: './' }
+		{ title: 'Buy Me A Coffee UwU', subtitle: 'For all the boolsh*t i do', link: './' },
+		{
+			title: 'Follow Me On The Internet..',
+			subtitle: `And watch me suffer everyplace ᵔ◡ᵔ`,
+			link: './'
+		}
 	];
 </script>
 
+<!-- general layer -->
 <main
-	id="layer-general"
 	class="overflow-auto w-screen h-screen bg-slate-50 text-slate-700 will-change-scroll scrollbar-hidden"
 >
 	<slot />
 </main>
+
+<!-- overlay layer -->
 <div
-	id="layer-overlay"
 	class="absolute top-0 left-0 pointer-events-none select-none w-screen h-screen overflow-hidden isolate contain-none"
 >
-	<div class="flex justify-between sticky top-0 z-40 snap-start p-2 pointer-events-none">
+	<div class="flex justify-between my-2 fixed top-0 z-40 w-full">
 		<nav
 			aria-labelledby="quickaccess-navigation"
-			class="overflow-x-auto overscroll-contain will-change-scroll no-scrollbar pointer-events-auto"
+			class="px-2 overflow-x-scroll will-change-scroll scrollbar-hidden pointer-events-auto"
 		>
-			<ul class="flex space-x-2">
+			<ul class="flex space-x-2 flex-nowrap w-fit">
 				{#each quickaccess_nav_items as { link, title }}
-					<!-- NOTE: as of 2022: wrapping li tags with anchors is totally acceptable -->
-					<a href={link}>
-						<li
-							class="truncate max-w-sm select-none bg-slate-50/95 border border-slate-200 px-2 rounded"
-						>
-							{title}
-						</li>
-					</a>
+					<li class="flex-initial">
+						<a href={link}>
+							<div
+								class="max-w-sm truncate select-none bg-slate-50/95 border border-slate-200 px-2 rounded"
+							>
+								<span>{title}</span>
+							</div>
+						</a>
+					</li>
 				{/each}
 			</ul>
 		</nav>
 		<nav
 			aria-labelledby="pinned-navigation"
-			class="overflow-x-auto overscroll-contain will-change-scroll no-scrollbar pointer-events-auto"
+			class="px-2 overflow-x-scroll will-change-scroll scrollbar-hidden pointer-events-auto"
 		>
-			<ul class="flex space-x-2">
+			<ul class="flex space-x-2 flex-nowrap w-fit">
 				{#each pinned_nav_items as { link, title }}
-					<a href={link}>
-						<li
-							class="truncate max-w-sm select-none bg-slate-50/95 border border-slate-200 px-2 rounded whitespace-nowrap"
-						>
-							{title}
-						</li>
-					</a>
+					<li class="flex-initial">
+						<a href={link}>
+							<div
+								class="max-w-sm truncate select-none bg-slate-50/95 border border-slate-200 px-2 rounded"
+							>
+								<span>{title}</span>
+							</div>
+						</a>
+					</li>
 				{/each}
 			</ul>
 		</nav>
 	</div>
-	<div class="snap-start p-2 flex flex-col space-y-2 fixed bottom-0 z-40 pointer-events-none">
+	<div class="flex flex-col space-y-2 my-2 fixed bottom-0 z-40 w-full">
 		<nav
 			aria-labelledby="recommended-navigation"
-			class="self-start overflow-x-auto overscroll-contain will-change-scroll no-scrollbar pointer-events-auto snap-x snap-proximity"
+			class="px-2 overflow-x-scroll will-change-scroll scrollbar-hidden snap-x snap-proximity scroll-px-2 pointer-events-auto"
 		>
-			<ul class="flex space-x-2">
+			<ul class="flex space-x-2 flex-nowrap w-fit">
 				{#each recommended_nav_items as { link, subtitle, title }}
-					<a href={link}>
-						<li
-							class="max-w-md truncate snap-start select-none bg-slate-50/95 border border-slate-200 pl-4 pr-12 py-2 rounded whitespace-nowrap"
-						>
-							<span class="font-semibold font-serif">{title}</span><br />
-							<span class="font-light">{subtitle}</span>
-						</li>
-					</a>
+					<li class="snap-start flex-initial">
+						<a href={link}>
+							<div
+								class="max-w-md truncate select-none bg-slate-50/95 border border-slate-200 pl-4 pr-12 py-2 rounded"
+							>
+								<span class="font-semibold font-serif">{title}</span><br />
+								<span class="font-light">{subtitle}</span>
+							</div>
+						</a>
+					</li>
 				{/each}
 			</ul>
 		</nav>
 		<nav
 			aria-labelledby="history-navigation"
-			class="self-start overflow-x-auto overscroll-contain will-change-scroll no-scrollbar pointer-events-auto"
+			class="px-2 overflow-x-scroll will-change-scroll scrollbar-hidden pointer-events-auto"
 		>
-			<ul class="flex space-x-2">
+			<ul class="flex space-x-2 flex-nowrap w-fit">
 				{#each history_nav_items as { link, title }}
-					<a href={link}>
-						<li
-							class="truncate max-w-sm select-none bg-slate-50/95 border border-slate-200 px-2 rounded whitespace-nowrap"
-						>
-							{title}
-						</li>
-					</a>
+					<li class="flex-initial">
+						<a href={link}>
+							<div
+								class="max-w-sm truncate select-none bg-slate-50/95 border border-slate-200 px-2 rounded"
+							>
+								<span>{title}</span>
+							</div>
+						</a>
+					</li>
 				{/each}
 			</ul>
 		</nav>
