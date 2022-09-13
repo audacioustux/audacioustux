7b5b867f551d34a8f8a98d5ac67b075aa20e7a09
Tanjim Hossain <tangimhossain1@gmail.com>
2022-09-13 19:52:12 +0600

    bare minimal

--- node-workspace/apps/web/src/routes/+layout.svelte
+++ node-workspace/apps/web/src/routes/+layout.svelte
@@ -3,29 +3,10 @@
 	import '@fontsource/work-sans/variable-full.css';
 	import '@fontsource/playfair-display/variable.css';
 	import '../app.postcss';
-
-	import NavWithSubtitle from '$lib/components/NavWithSubtitle.svelte';
-	import { useMachine } from '@xstate/svelte';
-	import { inspect } from '@xstate/inspect';
-	import { machine as appMachine, model as appModel } from '$lib/machines/+layout.machine';
-	import { browser, dev } from '$app/environment';
-	import Nav from '$lib/components/Nav.svelte';
-	import { pick } from 'radash';
 </script>
 
 <script lang="ts">
-	if (browser && dev) inspect({ iframe: false });
-
-	const machineOptions = { devTools: dev };
-	const { state, send, service } = useMachine(appMachine, machineOptions);
-
-	const {
-		children: { recommendedNav, historyNav, pinnedNav, relatedNav }
-	} = $state;
-
-	const onMousemove = (event: MouseEvent) => {
-		send(appModel.events.mouseMove(event));
-	};
+	const onMousemove = (event: MouseEvent) => {};
 </script>
 
 <svelte:window on:mousemove={onMousemove} />
@@ -36,25 +17,3 @@
 >
 	<slot />
 </main>
-
-<!-- overlay layer -->
-<div
-	class="absolute top-0 left-0 pointer-events-none select-none w-screen h-screen overflow-hidden isolate contain-none text-sm"
->
-	<div class="flex justify-between my-2 fixed top-0 z-40 w-full">
-		{#if $relatedNav}
-			<Nav {...pick($relatedNav.context, ['items', 'label'])} />
-		{/if}
-		{#if $pinnedNav}
-			<Nav {...pick($pinnedNav.context, ['items', 'label'])} />
-		{/if}
-	</div>
-	<div class="flex flex-col space-y-2 my-2 fixed bottom-0 z-40 w-full">
-		{#if $recommendedNav}
-			<NavWithSubtitle {...pick($recommendedNav.context, ['items', 'label', 'isVisible'])} />
-		{/if}
-		{#if $historyNav}
-			<Nav {...pick($historyNav.context, ['items', 'label'])} />
-		{/if}
-	</div>
-</div>
